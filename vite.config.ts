9cf426367c6431dded44c1bebeb6bfe3ce7755b3
maxrchung <maxrchung@gmail.com>
2023-06-13 14:18:04 -0700

    remove global thing?

--- vite.config.ts
+++ vite.config.ts
@@ -6,12 +6,6 @@ import svgrPlugin from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
-  define: {
-    // By default, Vite doesn't include shims for NodeJS/ necessary for segment
-    // analytics lib to work
-    // https://github.com/vitejs/vite/discussions/5912#discussioncomment-2908994
-    global: {},
-  },
   server: {
     open: true,
     port: 3000,
